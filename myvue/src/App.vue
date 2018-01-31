23457cb096e3f6ae6670e623146576ddfaec5be0
hanidea <hydavid@126.com>
2018-01-31 22:32:05 +0800

    myvue
    demo

--- myvue/src/App.vue
+++ myvue/src/App.vue
@@ -1,17 +1,69 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h1 v-text="title"></h1>
+    <input v-model="newItem" @keyup.enter="addNew">
+    <ul>
+      <li v-for="item in items " v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">{{item.label}}</li>
+    </ul>
+    <p>child tells me:{{childWords}}</p>
+    <component-a msgfromfather='my father' v-on:child-tell-me-something='listenToMyBoy'></component-a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+//import HelloWorld from './components/HelloWorld'
+import ComponentA from './components/ComponentA'
+import Store from './store'
+console.log(Store)
+// ComponentA.$on('tellme',function(){
+//   debugger;
+// })
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  //data:function()
+  data () {
+    return {
+      title: 'this is a to do list.',
+      items: Store.fetch(),
+      newItem:'',
+      childWords:''
+      
+    }
+  },
+  components: { ComponentA },
+  watch:{
+   items:{
+   handler:function(items){
+      //console.log(items)
+      Store.save(items)
+   },
+   deep:true //don't update status if false
+   }
+  },
+  // events: {
+  //   'child-tell-me-something': function (msg)
+  //   {
+  //     this.childWords = msg;
+  //   }
+  // },
+  methods:{
+    toggleFinish:function(item)
+      {
+        item.isFinished =!item.isFinished
+      },
+    addNew:function()
+    {
+      this.items.push({
+        label:this.newItem,
+        isFinished:false
+      })
+      //console.log(this.newItem),
+      this.newItem=""
+    },
+    listenToMyBoy:function(msg)
+    {
+      this.childWords = msg;
+    }
   }
 }
 </script>
@@ -24,5 +76,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.finished{
+text-decoration:underline;
 }
 </style>
---
+++ myvue/src/components/ComponentA.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+      <h1>{{msg}}</h1>
+      <h1>{{msgfromfather}}</h1>
+      <button v-on:click="onClickMe">open mouse</button>
+  </div>
+</template>
+<script>
+export default {
+    name: 'ComponentA',
+    data () {
+    return{
+        msg:'hello from component a!'
+    }
+},
+props: ['msgfromfather'],
+methods:{
+   onClickMe:function(){
+       //console.log(this.msgfromfather)
+       this.$emit('child-tell-me-something',this.msg);
+       //this.$dispatch('child-tell-me-something',this.msg);
+   }
+}
+}
+</script>
+<style>
+</style>
