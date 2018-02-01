e78b4eeb1510022d756f5378b0f5e93def35c2c4
hanidea <hydavid@126.com>
2018-02-01 10:31:00 +0800

    update App.vue
    component-a update componentA

--- myvue/src/App.vue
+++ myvue/src/App.vue
@@ -6,7 +6,7 @@
       <li v-for="item in items " v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">{{item.label}}</li>
     </ul>
     <p>child tells me:{{childWords}}</p>
-    <component-a msgfromfather='my father' v-on:child-tell-me-something='listenToMyBoy'></component-a>
+    <componentA msgfromfather='my father' v-on:child-tell-me-something='listenToMyBoy'></componentA>
   </div>
 </template>
 
