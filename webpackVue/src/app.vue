f4792620335743dde316bbdbd46ded640ff04428
hanidea <hydavid@126.com>
2018-05-18 09:12:29 +0800

    webpackVue
    test

---
+++ webpackVue/src/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="test">{{content}}</div>
+</template>
+
+<script>
+export default {
+    data: {
+        content: 'abc'
+    }
+}
+</script>
+
+<style>
+#test{
+    color: blue;
+}
+</style>
