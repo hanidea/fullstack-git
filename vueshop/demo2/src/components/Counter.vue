f352395f42d5c63b53bfabe4a7dd439ea390c89b
hanidea <hydavid@126.com>
2018-07-27 14:32:12 +0800

    vueshop
    vue

---
+++ vueshop/demo2/src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <button @click="increment">+</button>
+      <button v-on:click="decrement">-</button>
+      <p><span>{{num}}</span></p>
+  </div>
+</template>
+
+<script>
+export default {
+  props:["num"],
+  data () {
+    return {
+      num: '0',
+      msg: 'Vue.js App'
+    }
+  },
+  methods:{
+    increment(){
+      // this.num++;
+      this.$emit("incre")
+    },
+    decrement(){
+      // this.num--;
+      this.$emit("decre")
+    }
+  }
+}
+</script>
