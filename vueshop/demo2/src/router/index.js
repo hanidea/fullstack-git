d3224d38c0da3dd59ca1c489213dd85430db653b
hanidea <hydavid@126.com>
2018-07-27 16:34:48 +0800

    vueshop
    vuerouter

--- vueshop/demo2/src/router/index.js
+++ vueshop/demo2/src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+// import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '@/views/GoodsList'
+import Title from '@/views/Title'
+import Image from '@/views/Image'
+import Cart from '@/views/Cart'
 Vue.use(Router)
 
 export default new Router({
+  mode:'hash',
+  // mode:'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      // path: '/',
+      // name: 'HelloWorld',
+      // component: HelloWorld
+      // path: '/goods/:goodsId/user/:name',
+      path: '/goods/',
+      name: 'GoodsList',
+      component: GoodsList
+      // children:[
+      //   {
+      //     path:'title',
+      //     name:'title',
+      //     component:Title
+      //   },
+      //   {
+      //     path:'img',
+      //     name:'img',
+      //     component:Image
+      //   }
+      // ]
+    },
+    {
+      path:'/cart',
+      name:'cart',
+      component:Cart
     }
   ]
 })
---
+++ vueshop/demo2/src/views/GoodsList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      这是商品列表页面
+      <span>{{$route.params.goodsId}}</span><br>
+      <span>{{$route.params.name}}</span>
+      <router-link to="/goods/title">显示商品标题</router-link>
+      <router-link to="/goods/img">显示商品图片</router-link>
+      <router-view></router-view>
+      <!-- <router-link to="/cart">跳转到购物车页面</router-link> -->
+      <router-link v-bind:to="{name:'cart'}">跳转到购物车页面</router-link>
+      <button @click="jump">button - 跳转到购物车页面</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Vue.js App'
+    }
+  },
+  methods:{
+      jump(){
+        //   this.$router.push("/cart");
+        //   this.$router.push({path:'/cart?goodsId=123'});
+        this.$router.go(-2);
+      }
+  }
+}
+</script>
