b296f97eb7dfcf6c032384f6391d72d5b8750fa2
hanidea <hydavid@126.com>
2018-08-09 13:25:12 +0800

    vueshop
    cartlist

--- vueshop/demo2/server/routes/goods.js
+++ vueshop/demo2/server/routes/goods.js
@@ -17,10 +17,35 @@ mongoose.connection.on("error",function(){
 mongoose.connection.on("disconnected",function(){
     console.log("MongoDB connected disconnected.")
 })
-
+//查询商品列表
 router.get("/",function(req,res,next){
     //res.send('hello,goods list');
-    Goods.find({},function(err,doc){
+    let page = parseInt(req.param("page"));
+    let pageSize = parseInt(req.param("pageSize"));
+    let priceLevel = req.param("priceLevel");
+    var sort = req.param("sort");
+    let skip = (page-1)*pageSize;
+    let params = {};
+    var priceGt = '',priceLte = '';
+    if (priceLevel!="all"){
+        switch (priceLevel){
+            case '0':priceGt = 0;priceLte=500;break;
+            case '1':priceGt = 500;priceLte=1000;break;
+            case '2':priceGt = 1000;priceLte=5000;break;
+        }
+        params = {
+            salePrice:{
+                $gt:priceGt,
+                $lte:priceLte
+            }
+        }
+    }else{
+        
+    }
+  
+    let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+    goodsModel.sort({'salePrice':sort});
+    goodsModel.exec(function(err,doc){
         if(err){
             res.json({
                 status:'1',
@@ -38,5 +63,76 @@ router.get("/",function(req,res,next){
         }
     })
 });
+//加入到购物车
+router.post("/addCart",function(req,res,next){
+    var userId = '100000077';
+    var productId = req.body.productId;
+    var User = require('../models/users');
 
+    User.findOne({userId:userId},function(err,userDoc){
+        if(err){
+            res.json({
+                status:"1",
+                msg:err.message
+            });
+        }else{
+            console.log("userDoc:"+userDoc);
+            if(userDoc){
+                let goodsItem = '';
+                userDoc.cartList.forEach(function(item){
+                    if(item.productId == productId){
+                        goodsItem = item;
+                        item.productNum ++;
+                    }
+                });
+                if(goodsItem){
+                    userDoc.save(function(err2,doc2){
+                        if(err2){
+                            res.json({
+                                status:"1",
+                                msg:err2.message
+                            });
+                        }else{
+                            res.json({
+                                status:"0",
+                                msg:'newmsg2',
+                                result:'success2'
+                            });
+                        }
+                    });
+                }else{
+                    Goods.findOne({productId:productId},function(err1,doc1){
+                        if(err1){
+                            res.json({
+                                status:"1",
+                                msg:err1.message
+                            });
+                        }else{
+                            if(doc1){
+                                var obj = doc1.toObject();
+                                obj.productNum = 1;
+                                obj.checked = 1;
+                                userDoc.cartList.push(obj);
+                                userDoc.save(function(err2,doc2){
+                                    if(err2){
+                                        res.json({
+                                            status:"1",
+                                            msg:err2.message
+                                        });
+                                    }else{
+                                        res.json({
+                                            status:"0",
+                                            msg:'newmsg',
+                                            result:'success'
+                                        });
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    });
+});
 module.exports = router;
--- vueshop/demo2/src/views/GoodsList.vue
+++ vueshop/demo2/src/views/GoodsList.vue
@@ -9,7 +9,7 @@
             <div class="filter-nav">
               <span class="sortby">Sort by:</span>
               <a href="javascript:void(0)" class="default cur">Default</a>
-              <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+              <a @click="sortGoods" href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
               <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
             </div>
             <div class="accessory-result">
@@ -48,11 +48,15 @@
                         <div class="name">{{item.productName}}</div>
                         <div class="price">{{item.salePrice}}</div>
                         <div class="btn-area">
-                          <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                          <a href="javascript:;" class="btn btn--m" @click="addCart(item.productId)">加入购物车</a>
                         </div>
                       </div>
                     </li>
                   </ul>
+                  <div class="load-more" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
+                    <!-- 加载中... -->
+                    <img src="./../assets/loading-spinning-bubbles.svg" v-show="loading"/>
+                  </div>
                 </div>
               </div>
             </div>
@@ -74,6 +78,11 @@ export default {
   data () {
     return {
       goodsList:[],
+      sortFlag:true,
+      page:1,
+      pageSize:8,
+      busy:true,
+      loading:false,
       priceFilter:[
         {
           startPrice:'0,00',
@@ -85,7 +94,7 @@ export default {
         },
         {
           startPrice:'1000.00',
-          endPrice:'2000.00'
+          endPrice:'5000.00'
         }
       ],
       priceChecked:'all',
@@ -100,10 +109,57 @@ export default {
     this.getGoodsList();
   },
   methods:{
-    getGoodsList(){
-      axios.get("/goods").then((result)=>{
-        var res = result.data.result.list;
-        this.goodsList = res;
+    getGoodsList(flag){
+      var param = {
+        page:this.page,
+        pageSize:this.pageSize,
+        sort:this.sortFlag?1:-1,
+        priceLevel:this.priceChecked
+      }
+      this.loading = true
+      axios.get("/goods",{
+        params:param
+      }).then((result)=>{
+        var res = result.data;
+        this.loading = false
+        if(res.status=="0"){
+          if(flag){
+            this.goodsList = this.goodsList.concat(res.result.list);
+            if(res.result.count==0){
+              this.busy = true;
+            }else{
+              this.busy = false;
+            }
+          }else{
+            this.goodsList = res.result.list;
+            this.busy = false;
+          }
+        }else{
+          this.goodsList = [];
+        }
+      })
+    },
+    sortGoods(){
+      this.sortFlag = ! this.sortFlag;
+      this.page = 1,
+      this.getGoodsList();
+    },
+    loadMore(){
+      this.busy = true;
+      setTimeout(() => {
+           this.page++;
+           this.getGoodsList(true);
+        }, 500);
+    },
+    addCart(productId){
+      axios.post("/goods/addCart",{
+        productId:productId
+      }).then((res)=>{
+        if(res.data.status=="0"){
+          alert("加入成功")
+        }else{
+          alert("msg:"+res)
+        }
       })
     },
     showFilterPop(){
@@ -112,6 +168,8 @@ export default {
     },
     setPriceFilter(index){
       this.priceChecked=index,
+      this.getGoodsList(),
+      this.page = 1;
       this.closePop()
     },
     closePop(){
