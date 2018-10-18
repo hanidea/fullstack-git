ceebe5cb175a949dcfe61e43f99bef05ed6122f2
hanidea <hydavid@126.com>
2018-10-18 20:05:09 +0800

    ebook
    ebook

---
+++ ebook-demo/src/Ebook.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="ebook">
+        <div class="read-wrapper">
+            <div id="read"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Epub from 'epubjs'
+const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
+global.ePub = Epub
+export default {
+  methods: {
+    // 电子书的解析和渲染
+    showEpub () {
+      // 生成Book
+      this.book = new Epub(DOWNLOAD_URL)
+      // console.log(this.book)
+      // 生成Rendition，通过Book.renderTo
+      this.rendition = this.book.renderTo('read', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      // 通过Rendition.display渲染电子书
+      this.rendition.display()
+    }
+  },
+  mounted () {
+    this.showEpub()
+  }
+}
+</script>
+
+<style lang='scss' scoped='' type='text/css'>
+    @import 'assets/styles/global';
+</style>
