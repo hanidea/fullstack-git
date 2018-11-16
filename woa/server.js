5b981197cf977007d9114bbe2fbb084ea71327d0
hanidea <hydavid@126.com>
2018-11-16 13:47:07 +0800

    woa
    woa

---
+++ woa/server.js
@@ -0,0 +1,24 @@
+const Woa = require('./application')
+const app = new Woa()
+
+app.use((req,res)=>{
+    res.writeHead(200)
+    res.end('hello James')
+})
+app.listen(9092,()=>{
+    console.log('server runing on port 9092')
+})
+
+
+
+
+
+
+// const http = require('http')
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200)
+//     res.end('hello James')
+// })
+// server.listen(9092,()=>{
+//     console.log('server start on port 9092')
+// })
