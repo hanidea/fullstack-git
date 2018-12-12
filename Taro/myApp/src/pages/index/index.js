4fb470c1e00751c6102a8afa340f33ad6ea62c6b
hanidea <hydavid@126.com>
2018-12-12 13:54:14 +0800

    update
    child.js

--- Taro/myApp/src/pages/index/index.js
+++ Taro/myApp/src/pages/index/index.js
@@ -1,17 +1,19 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import './index.less'
-
+import Child from './Child';
 export default class Index extends Component {
 
   config = {
     navigationBarTitleText: '首页'
   }
-
+  state={
+    name:"李四"
+  }
   componentWillMount () { }
 
   componentDidMount () { 
-    this.setState({name:"李四"})
+    // this.setState({name:"李四"})
   }
 
   componentWillUnmount () { }
@@ -19,11 +21,21 @@ export default class Index extends Component {
   componentDidShow () { }
 
   componentDidHide () { }
-
+  click(){
+    this.setState({name:"张三"},()=>{
+      console.log(this.state.name)
+    });
+   
+  }
+  change(){
+    this.setState({name:'改变name值'})
+  }
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <Child name={this.state.name} onchange={this.change.bind(this)}/>
+        <Button onClick={this.click}>改变名称</Button>
+        <Text>{this.state.name}</Text>
       </View>
     )
   }
