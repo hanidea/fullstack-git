51e023a3192a99418a6451de06d045d7863ea2a8
hanidea <hydavid@126.com>
2018-12-12 11:12:20 +0800

    Taro
    demo

---
+++ Taro/myApp/src/pages/index/index.js
@@ -0,0 +1,31 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.less'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { 
+    this.setState({name:"李四"})
+  }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
+
