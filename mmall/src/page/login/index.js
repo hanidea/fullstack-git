06ae5e532c9ddb869cb2f3dd833c772103f8c0f4
hanidea <hydavid@126.com>
2018-12-18 16:34:20 +0800

    common

---
+++ mmall/src/page/login/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: James 
+ * @Date: 2018-12-18 15:47:41 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-12-18 16:14:54
+ */
+var $$ = require('jquery');
+console.log('hello login');
+$$('body').html('hello ~~~~~~');
--- mmall/webpack.config.js
+++ mmall/webpack.config.js
@@ -1,10 +1,37 @@
+/*
+ * @Author: James 
+ * @Date: 2018-12-18 15:17:08 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-12-18 16:24:53
+ */
+const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
+var config = {
   mode: 'production',
-  entry: './src/page/index/index.js',
+  entry: {
+    'index':['./src/page/index/index.js'],
+    'login':['./src/page/login/index.js']
+  },
   output: {
-    filename: 'main.js',
+    filename: 'js/[name].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  externals:{
+    'jquery': 'window.jQuery'
+  },
+  plugins: [],
+  optimization: {
+    splitChunks: {
+        cacheGroups: {
+            commons: {
+                name: "commons",
+                chunks: "initial",
+                minChunks: 2
+            }
+        }
+    }
+},
 };
+
+module.exports = config;
