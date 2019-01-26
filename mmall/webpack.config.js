c5ca661853709c41c5ab0c60360977e3d5c42c91
hanidea <hydavid@126.com>
2019-01-26 17:17:23 +0800

    cart
    cart

--- mmall/webpack.config.js
+++ mmall/webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: James 
  * @Date: 2018-12-18 15:17:08 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2019-01-25 16:06:27
+ * @Last Modified time: 2019-01-26 17:01:56
  */
 
 const path = require('path');
@@ -33,6 +33,7 @@ var config = {
     'index':['./src/page/index/index.js'],
     'list': ['./src/page/list/index.js'],
     'detail': ['./src/page/detail/index.js'],
+    'cart': ['./src/page/cart/index.js'],
     'user-login':['./src/page/user-login/index.js'],
     'user-register':['./src/page/user-register/index.js'],
     'user-center':['./src/page/user-center/index.js'],
@@ -117,6 +118,7 @@ var config = {
     new HtmlWebpackPlugin(getHtmlConfig('index','首页')),
     new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表页')),
     new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情页')),
+    new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
     new HtmlWebpackPlugin(getHtmlConfig('user-login','用户登录')),
     new HtmlWebpackPlugin(getHtmlConfig('user-register','用户注册')),
     new HtmlWebpackPlugin(getHtmlConfig('user-center','个人中心')),
