45ab4f7b08d3d04f5ac4b54f432f8ce3262d186d
hanidea <hydavid@126.com>
2019-02-01 17:46:38 +0800

    order-confirm
    order-confirm

--- mmall/webpack.config.js
+++ mmall/webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: James 
  * @Date: 2018-12-18 15:17:08 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2019-02-01 16:29:18
+ * @Last Modified time: 2019-02-01 17:11:12
  */
 
 const path = require('path');
@@ -34,7 +34,7 @@ var config = {
     'list': ['./src/page/list/index.js'],
     'detail': ['./src/page/detail/index.js'],
     'cart': ['./src/page/cart/index.js'],
-    'confirm': ['./src/page/confirm/index.js'],
+    'order-confirm': ['./src/page/order-confirm/index.js'],
     'user-login':['./src/page/user-login/index.js'],
     'user-register':['./src/page/user-register/index.js'],
     'user-center':['./src/page/user-center/index.js'],
@@ -120,7 +120,7 @@ var config = {
     new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表页')),
     new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情页')),
     new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
-    new HtmlWebpackPlugin(getHtmlConfig('confirm', '订单确认页')),
+    new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认页')),
     new HtmlWebpackPlugin(getHtmlConfig('user-login','用户登录')),
     new HtmlWebpackPlugin(getHtmlConfig('user-register','用户注册')),
     new HtmlWebpackPlugin(getHtmlConfig('user-center','个人中心')),
