d855b429b897511452c548cc43b4155493d8dd28
hanidea <hydavid@126.com>
2019-03-19 16:09:34 +0800

    mt-app

--- koa2first/routes/index.js
+++ koa2first/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
+  global.console.log('index2')
+  ctx.cookies.set('pvid',Math.random())
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
@@ -12,8 +14,29 @@ router.get('/string', async (ctx, next) => {
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
+    title: 'koa2 json',
+    cookie: ctx.cookies.get('pvid')
   }
 })
-
+router.get('/testAsync',async(ctx)=>{
+  global.console.log('start',new Date().getTime())
+  const a = await new Promise((resolve,reject)=>{
+    setTimeout(function(){
+      global.console.log('async a',new Date().getTime())
+      resolve('ab')
+    },1000);
+  })
+  const b = await Promise.resolve(12)
+  const c = await new Promise((resolve,reject)=>{
+    setTimeout(function(){
+      global.console.log('async c',new Date().getTime())
+      resolve('abc')
+    },2000);
+  })
+  ctx.body={
+    a,
+    b,
+    c
+  }
+})
 module.exports = router
