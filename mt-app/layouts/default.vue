f48af68776ba5988d0ad630ba703d8e26c8091bb
hanidea <hydavid@126.com>
2019-03-23 14:12:56 +0800

    dbs
    update server

--- mt-app/layouts/default.vue
+++ mt-app/layouts/default.vue
@@ -4,14 +4,16 @@
     <my-header/>
   </el-header>
   <el-main> <nuxt /></el-main>
-  <el-footer>Footer</el-footer>
+  <el-footer height="100%"><my-footer/></el-footer>
   </el-container>
 </template>
 <script>
 import MyHeader from '@/components/public/header/index.vue'
+import MyFooter from "@/components/public/footer/index"
 export default {
   components:{
-    MyHeader
+    MyHeader,
+    MyFooter
   }
 }
 </script>
