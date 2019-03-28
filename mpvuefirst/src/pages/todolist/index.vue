c43f3351cd34e1f7e0830a969f80e6a04c4bf529
hanidea <hydavid@126.com>
2019-03-28 09:08:54 +0800

    mpvuefirst
    first

---
+++ mpvuefirst/src/pages/todolist/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>vuejs</h1>
+        <Todolist></Todolist>
+    </div>
+</template>
+<script>
+import Todolist from '@/components/Todolist'
+export default {
+  components: {
+    Todolist
+  }
+}
+</script>
+<style scoped>
+h1{
+    color:red;
+}
+</style>
+
