3276959e12abe781006f22f982741303cf67e2e8
hanidea <hydavid@126.com>
2019-03-29 15:15:55 +0800

    vuenativefirst
    vue

--- mpvuefirst/src/main.js
+++ mpvuefirst/src/main.js
@@ -2,7 +2,18 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
-App.mpType = 'app'
 
 const app = new Vue(App)
 app.$mount()
+
+export default{
+  config: {
+    pages: [],
+    'window': {
+      'backgroundTextStyle': 'light',
+      'navigationBarBackgroundColor': '#EA5149',
+      'navigationBarTitleText': '蜗牛图书',
+      'navigationBarTextStyle': 'light'
+    }
+  }
+}
