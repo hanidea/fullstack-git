ab71d3152d4ce3d6e9d403333352d4cb00a19c5d
hanidea <hydavid@126.com>
2019-04-02 17:31:54 +0800

    mpvuefirst
    test

--- mpvuefirst/src/App.vue
+++ mpvuefirst/src/App.vue
@@ -1,9 +1,10 @@
 
 <script>
+import {get} from './util'
 export default {
   async created () {
-    // const res = await get('/weapp/demo')
-    // console.log(123, res)
+    const res = await get('/weapp/demo')
+    console.log(123, res)
     // wx.request({
     //   url:config.host+'/weapp/demo',
     //   success:function(res){
