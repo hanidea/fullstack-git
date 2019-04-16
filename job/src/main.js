8539dbdac8affe13b5d75c4de4d05d124c74423a
hanidea <hydavid@126.com>
2019-04-16 21:36:19 +0800

    job
    job

---
+++ job/src/main.js
@@ -0,0 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import JKUI from 'jk-ui'
+import 'jk-ui/theme-default/lib/index.css'
+Vue.use(JKUI)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
