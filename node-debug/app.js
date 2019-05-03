7aad48bc379af9ded0278e16d2a5bf6cd289a2b7
hanidea <hydavid@126.com>
2019-05-03 11:26:40 +0800

    cmd
    --inspect

--- node-debug/app.js
+++ node-debug/app.js
@@ -13,15 +13,18 @@ const express = require('express')
 const app = express()
 //command + shift + p 打开auto attach
 app.get('/', (req, res) => {
-    let a = 123;
-    function b(){
-        a +=1;
-    }
-    function c(){
-        b();
-    }
-    c();
-    res.send(`Hello World! ${a}`)
+    // let a = 123;
+    // function b(){
+    //     while(a< 10**9){
+    //         a +=1;
+    //     }
+    // }
+    // // function c(){
+    // //     b();
+    // // }
+    // // c();
+    // b();
+    res.send('Hello World!')
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
