1e6aa46d58e9bcc97eb886e32d2753e5ff5d7d85
hanidea <hydavid@126.com>
2019-07-11 14:42:00 +0800

    Create let-const.js
    let-const

---
+++ es6demo/let-const.js
@@ -0,0 +1,45 @@
+//变量和常量
+let r = 2;
+r = 4;
+console.log(r);
+
+const pi = 3.1415926;
+pi = 10;
+
+//不能重复定义
+var foo = 1;
+var foo = 2;
+console.log(foo);
+
+let bar = 1;
+let bar = 2;
+console.log(bar);
+
+//块级作用域
+if(true){
+    var test = 1;
+}
+console.log(test);
+
+if(true){
+    let test1 = 2;
+}
+console.log(test1);
+
+var arr = [1,2,3,4];
+for(var i=0, iLength = arr.length; i<iLength; i++){
+
+}
+console.log(i);
+
+let = [1,2,3,4];
+for(let i=0, iLength = arr.length; i<iLength; i++){
+
+}
+console.log(i);
+
+//不存在变量提升
+console.log(foo);
+var foo = 1;
+console.log(bar);
+let bar = 1;
