bac091537f436d16b618db28077f23c2cf3bad54
hanidea <hydavid@126.com>
2019-07-13 09:54:46 +0800

    modules

---
+++ es6demo/cookie-session.js
@@ -0,0 +1,11 @@
+//查看 cookie
+document.cookie
+
+//添加 cookie
+document.cookie = 'name=James;domain=happymmall.com;path=/index.html;expires=Sat, 13 Jul 2019 01:45:04 GMT'
+
+//修改 cookie
+document.cookie = 'name=James1;domain=happymmall.com;path=/index.html'
+
+//删除 cookie
+document.cookie = 'name=James1;domain=happymmall.com;path=/index.html;expires=Sat, 13 Jul 2017 01:45:04 GMT'
