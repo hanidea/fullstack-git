d05cb03c8d7685d130549607f691aecf5a0b0466
hanidea <hydavid@126.com>
2019-07-14 10:00:48 +0800

    dist

--- admin-v1/webpack.config.js
+++ admin-v1/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
@@ -6,7 +7,8 @@ module.exports = {
   entry: './src/app.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.js'
+    publicPath: '/dist/',
+    filename: 'js/app.js'
   },
   module: {
     rules: [
@@ -36,13 +38,50 @@ module.exports = {
             fallback: "style-loader",
             use: ["css-loader",'sass-loader']
           })
+      },
+      //图片配置
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'resource/[name].[ext]'
+            }
+          }
+        ]
+      },
+      //字体图标配置
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'resource/[name].[ext]'
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
+      //处理html文件
       new HtmlWebpackPlugin({
           template:'./src/index.html'
       }),
-      new ExtractTextPlugin("index.css"),
-  ]
+      //独立css文件
+      new ExtractTextPlugin("css/[name].css"),
+      //提取公共模块
+      new webpack.optimize.CommonsChunkPlugin({
+          name:'common',
+          filename:'js/base.js'
+      })
+
+  ],
+  devServer: {
+    port:8086
+  },
 };
