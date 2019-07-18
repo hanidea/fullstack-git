6421efcdbe204bf0ce619aa09734ccb86c5a0c69
hanidea <hydavid@126.com>
2019-07-18 16:35:19 +0800

    admin-v1

---
+++ admin-v1/src/component/top-nav/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class TopNav extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <div className="navbar navbar-default top-navbar" role="navigation">
+            <div className="navbar-header">
+                {/* <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button> */}
+                <Link className="navbar-brand" to="/"><b>admin</b>V1</Link>
+            </div>
+
+            <ul className="nav navbar-top-links navbar-right">
+                <li className="dropdown">
+                    <a className="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <i className="fa fa-user fa-fw"></i><span>欢迎，adminV1</span><i className="fa fa-caret-down"></i>
+                    </a>
+                    <ul className="dropdown-menu dropdown-user">
+                        <li><a href="#"><i className="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i className="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li className="divider"></li>
+                        <li><a href="#"><i className="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        )
+    }
+}
+
+export default TopNav;
