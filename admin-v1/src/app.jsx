643524488fc9304d00f4cb46a26c99307b99bb81
hanidea <hydavid@126.com>
2019-07-23 10:50:14 +0800

    index

--- admin-v1/src/app.jsx
+++ admin-v1/src/app.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route, Link, Switch,Redirect} from 'react-route
 import Layout from 'component/layout/index.jsx';
 //页面
 import Home from 'page/home/index.jsx';
+import ProductRouter from 'page/product/route.jsx';
 import Login from 'page/login/index.jsx';
 import UserList  from 'page/user/index.jsx';
 import ErrorPage from 'page/error/index.jsx';
@@ -15,8 +16,8 @@ class App extends React.Component{
       <Layout>
           <Switch>
               <Route exact path="/" component={Home}/>
-                 <Route path="/product" component={Home}/>
-                    <Route path="/product-category" component={Home}/>
+                 <Route path="/product" component={ProductRouter}/>
+                    <Route path="/product-category" component={ProductRouter}/>
                     <Route path="/user/index" component={UserList}/>
                     <Redirect exact from="/user" to="/user/index"/>
                     <Route component={ErrorPage}/>
