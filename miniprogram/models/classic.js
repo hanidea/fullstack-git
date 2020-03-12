0a1f5bfe480b0da7f4c33dcab95742d4044b8be6
hanidea <hydavid@126.com>
2020-03-12 17:15:34 +0800

    classic

---
+++ miniprogram/models/classic.js
@@ -0,0 +1,11 @@
+import {HTTP} from '../util/http.js'
+class ClassicModel extends HTTP{
+  getLatest(){
+    this.request({
+      url: 'classic/latest',
+      success: (res) => {
+        console.log(res)
+      }
+    })
+  }
+}
--- miniprogram/util/http.js
+++ miniprogram/util/http.js
@@ -1,6 +1,11 @@
 import {
   config
 } from '../config.js'
+const tips ={
+  1:'抱歉,出现一个错误',
+  1005:'appkey无效，请申请',
+  3000:'期刊不存在'
+}
 class HTTP {
   request(params) {
     //url, data, method,
@@ -20,18 +25,25 @@ class HTTP {
         if (code.startsWith('2')) {
           params.success(res.data)
         } else {
-          wx.showToast({
-            title: '错误',
-            icon:'none',
-            duration:'2000'
-          })
+          let error_code = res.data.error_code
+          this._show_error(error_code)
         }
       },
       fail: (err) => {
-
+        this._show_error(1)
       }
     })
 
+  }
+  _show_error(error_code){
+    if(!error_code){
+      error_code = 1
+    }
+    wx.showToast({
+      title: tips[error_code],
+      icon: 'none',
+      duration: 2000
+    })
   }
 }
 
