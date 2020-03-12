1eff393be4b02476de638fa96526845bc2ecc81b
hanidea <hydavid@126.com>
2020-03-12 13:04:45 +0800

    like

--- miniprogram/components/like/index.js
+++ miniprogram/components/like/index.js
@@ -11,6 +11,11 @@ Component({
    * 组件的初始数据
    */
   data: {
+    like:false,
+    count1:99,
+    count2:999,
+    yesSrc:'images/like.png',
+    noSrc:'images/like@dis.png',
 
   },
 
@@ -18,6 +23,8 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onLike:function(event){
+      console.log(event)
+    }
   }
 })
