4dac7d039c8e382b400a6d4c11f001ab77a8b5f4
hanidea <hydavid@126.com>
2020-03-21 11:51:00 +0800

    pagination

--- miniprogram/components/search/index.js
+++ miniprogram/components/search/index.js
@@ -1,16 +1,18 @@
 // components/search/index.js
 import {KeywordModel} from '../../models/keyword.js'
 import { BookModel } from '../../models/book.js'
+import { paginationBev} from '../behaviors/pagination.js'
 const keywordModel = new KeywordModel()
 const bookModel = new BookModel()
 Component({
   /**
    * 组件的属性列表
    */
+  behaviors:[paginationBev],
   properties: {
     more:{
       type:String,
-      observer:'_load_more'
+      observer:'loadMore'
     }
   },
 
@@ -20,7 +22,7 @@ Component({
   data: {
     historyWords:[],
     hotWords:[],
-    dataArray:[],
+    //dataArray:[],
     searching:false,
     q:'',
     loading:false
@@ -43,8 +45,7 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    _load_more(){
-      console.log(123123)
+    loadMore(){
       if(!this.data.q){
         return
       }
@@ -52,15 +53,18 @@ Component({
         return
       }
       //锁
-      const length = this.data.dataArray.length
+      //const length = this.data.dataArray.length
+      if(this.hasMore()){
       this.data.loading = true
-      bookModel.search(length,this.data.q).then(res=>{
-        const tempArray = this.data.dataArray.concat(res.books)
-        this.setData({
-          dataArray:tempArray,
-        })
+      bookModel.search(this.getCurrentStart(),this.data.q).then(res=>{
+        //const tempArray = this.data.dataArray.concat(res.books)
+        this.setMoreData(res.books)
+        // this.setData({
+        //   dataArray:tempArray,
+        // })
         this.data.loading = false
       })
+      }
     },
     onCancel(event){
       this.triggerEvent('cancel',{},{})
@@ -74,10 +78,13 @@ Component({
       this.setData({
         searching:true
       })
+      this.initialize()
       const q = event.detail.value || event.detail.text
       bookModel.search(0,q).then(res=>{
+        this.setMoreData(res.books)
+        this.setTotal(res.total)
         this.setData({
-          dataArray:res.books,
+          //dataArray:res.books,
           q
         })
         keywordModel.addToHistory(q)
