b76b9e33388181a54d59a862d7a5957a343527c7
hanidea <hydavid@126.com>
2020-03-22 17:13:45 +0800

    koa

--- miniKoa/app.js
+++ miniKoa/app.js
@@ -2,16 +2,17 @@ const Koa = require('koa')
 
 const app = new Koa()
 
-app.use((ctx, next)=>{
-    console.log(1)
-    next()
-    console.log(2)
+app.use(async (ctx, next)=>{
+    await next()
+    const r = ctx.r
+    console.log(r)
 })
 
-app.use((ctx, next)=>{
-    console.log(3)
-    next()
-    console.log(4)
+app.use(async (ctx, next)=>{
+    const axios = require('axios')
+    const res = await axios.get('https://www.bighanhan.com')
+    ctx.r = res
+    await next()
 })
 
 
