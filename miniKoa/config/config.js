a6463ffc8cad87ab1b96ac4d44116f880ee631f5
hanidea <hydavid@126.com>
2020-04-04 16:11:32 +0800

    minikoa

--- miniKoa/config/config.js
+++ miniKoa/config/config.js
@@ -1,19 +1,24 @@
 module.exports = {
-    environment:'dev',
-    database: {
-        dbName: 'island',
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'Junjie2020!'
-      },
-    security: {
-        secretKey: "abcdefg",
-        expiresIn: 60 * 60 * 24 * 30
-      },
-    wx: {
-        appId: 'wx434a0eb05adde3d6',
-        appSecret: '',
-        loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
-      },
+  environment: 'dev',
+  database: {
+    dbName: 'island',
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Junjie2020!'
+  },
+  security: {
+    secretKey: "abcdefg",
+    expiresIn: 60 * 60 * 24 * 30
+  },
+  wx: {
+    appId: 'wx434a0eb05adde3d6',
+    appSecret: '1522bd1a054925e880dfe959887c4b24',
+    loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
+  },
+  yushu: {
+    detailUrl: 'http://t.yushu.im/v2/book/id/%s',
+    keywordUrl: 'http://t.yushu.im/v2/book/search?q=%s&count=%s&start=%s&summary=%s'
+  },
+  host:'http://localhost:3000/'
 }
--- miniKoa/core/util.js
+++ miniKoa/core/util.js
@@ -7,15 +7,12 @@ const findMembers = function (instance, {
     specifiedType,
     filter
 }) {
-    // 递归函数
     function _find(instance) {
-        //基线条件（跳出递归）
         if (instance.__proto__ === null)
             return []
 
         let names = Reflect.ownKeys(instance)
         names = names.filter((name) => {
-            // 过滤掉不满足条件的属性或方法名
             return _shouldKeep(name)
         })
 
