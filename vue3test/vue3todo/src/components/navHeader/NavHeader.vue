6181c1262943997b8beb741c1731fe028500ec26
hanidea <hydavid@126.com>
2021-05-19 13:38:16 +0800

    scss

--- vue3test/vue3todo/src/components/navHeader/NavHeader.vue
+++ vue3test/vue3todo/src/components/navHeader/NavHeader.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-      <input placeholder="请输入任务名称" v-model="value"/>
+      <input placeholder="请输入任务名称" 
+      v-model="value"
+      @keydown.enter="enter"/>
   </div>
 </template>
 <script>
@@ -9,13 +11,19 @@ export default defineComponent({
   name:'navHeader',
   setup(){
     let value = ref('')
+    let enter = () => {
+      console.log(value.value)
+    }
     return {
-      value
+      value,
+      enter
     }
 
   }
 })
 </script>
 <style scoped lang="scss">
-
+  input {
+    margin-bottom:10px;
+  }
 </style>
--- vue3test/vue3todo/src/store/index.js
+++ vue3test/vue3todo/src/store/index.js
@@ -3,10 +3,14 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
   },
+  //第一个参数state 第二个参数是需要修改的值
   mutations: {
   },
+  //异步提交mutation
+  //第一个参数是store 第二个参数是修改的值
   actions: {
   },
+  // 模块化
   modules: {
   }
 })
