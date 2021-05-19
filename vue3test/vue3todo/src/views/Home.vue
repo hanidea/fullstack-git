1eeb70446f79e8774dc2aea0733a2307f48cc52f
hanidea <hydavid@126.com>
2021-05-19 10:18:53 +0800

    Update Home.vue

--- vue3test/vue3todo/src/views/Home.vue
+++ vue3test/vue3todo/src/views/Home.vue
@@ -1,9 +1,8 @@
 <template>
-  <div>
-    <nav-header></nav-header>
-    <nav-main></nav-main>
-    <nav-footer></nav-footer>
-  </div>
+      <div>{{num}}</div>
+      <div>{{name}}</div>
+      <div>{{arr.slice(0,2)}}</div>
+      <div>{{obj.age}}</div>
 </template>
 <script>
 import NavHeader from '@/components/navHeader/NavHeader'
@@ -21,9 +20,16 @@ export default defineComponent({
   },
   setup(props,ctx){
     let num = ref(10)
-    
+    let name = ref('jack')
+    let arr = ref(['a','b','c','d'])
+    let obj = ref({
+      age:20
+    })
     return{
-
+      num,
+      name,
+      arr,
+      obj
     }
   }
 
