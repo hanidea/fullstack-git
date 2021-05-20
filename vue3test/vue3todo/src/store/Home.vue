7303fb7f977e58b0432a6bba09fa22c1d643533f
hanidea <hydavid@126.com>
2021-05-20 10:46:00 +0800

    版本4

--- vue3test/vue3todo/src/store/Home.vue
+++ vue3test/vue3todo/src/store/Home.vue
@@ -8,7 +8,7 @@
       <div>{{list}}</div> -->
       <button @click="goto">跳转路由</button>
       <div>{{name}}------{{num}}------{{obj}}</div>
-      <div>版本3</div>
+      <div>版本4</div>
 </template>
 <script>
 import NavHeader from '@/components/navHeader/NavHeader'
