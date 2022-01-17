26987a12945319fd320851a85e8a7ef8dbf1de54
hanidea <hydavid@126.com>
2022-01-17 15:09:45 +0800

    Update config.js

--- miniKoa/config/config.js
+++ miniKoa/config/config.js
@@ -17,8 +17,10 @@ module.exports = {
     loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
   },
   yushu: {
-    detailUrl: 'http://t.yushu.im/v2/book/id/%s',
-    keywordUrl: 'http://t.yushu.im/v2/book/search?q=%s&count=%s&start=%s&summary=%s'
+    detailUrl: 'http://t.talelin.com/v2/book/id/%s',
+    keywordUrl: 'http://t.talelin.com/v2/book/search?q=%s&count=%s&start=%s&summary=%s'
+    // detailUrl: 'http://t.yushu.im/v2/book/id/%s',
+    // keywordUrl: 'http://t.yushu.im/v2/book/search?q=%s&count=%s&start=%s&summary=%s'
   },
   host:'http://127.0.0.1:3000/'
 }
